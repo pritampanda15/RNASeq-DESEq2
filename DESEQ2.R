53d3ee39dd5a44d06ed43ba93a1d853a06c96899
Pritam Kumar Panda <pritampkp15@gmail.com>
2023-02-15 17:51:32 +0100

    Add files via upload

---
+++ DESEQ2.R
@@ -0,0 +1,166 @@
+library(DESeq2)
+setwd("~/deseq2")
+rawCounts <- read.delim("E-GEOD-54456-raw-counts.tsv")
+sampleData <- read.delim("design.tsv")
+sampleData_v2 <- sampleData
+geneID <- rawCounts$Gene.ID
+sampleIndex <- grepl("SRR\\d+", colnames(rawCounts))
+rawCounts <- as.matrix(rawCounts[,sampleIndex])
+rownames(rawCounts) <- geneID
+head(rawCounts)
+rawCounts <- as.data.frame(rawCounts)
+
+head(sampleData)
+rownames(sampleData) <- sampleData$Run
+keep <- c("Sample.Characteristic.disease.", "Sample.Characteristic.phenotype.")
+sampleData <- sampleData[,keep]
+colnames(sampleData) <- c("tissueType", "individualID")
+sampleData$individualID <- factor(sampleData$individualID)
+head(sampleData)
+sampleData <- as.data.frame(sampleData)
+
+rawCounts <- rawCounts[,unique(rownames(sampleData))]
+all(colnames(rawCounts) %in% rownames(sampleData))
+all(colnames(rawCounts) == rownames(sampleData))
+
+rename_tissues <- function(x){
+  x <- switch(as.character(x), "normal"="healthy_subject", "lesional skin"= "diseased_state" )
+  return(x)
+}
+sampleData$individualID <- unlist(lapply(sampleData$individualID, rename_tissues))
+
+sampleData$individualID <- factor(sampleData$individualID, levels=c("healthy_subject", "diseased_state"))
+
+
+
+deseq2Data <- DESeqDataSetFromMatrix(countData=rawCounts, colData=sampleData, design= ~individualID)
+
+dim(deseq2Data)
+dim(deseq2Data[rowSums(counts(deseq2Data)) > 5, ])
+deseq2Data <- deseq2Data[rowSums(counts(deseq2Data)) > 5, ]
+
+library(BiocParallel)
+register(MulticoreParam(4))
+
+deseq2Data <- DESeq(deseq2Data, modelMatrixType = T, full = design(deseq2Data))
+
+deseq2Results <- results(deseq2Data)
+summary(deseq2Results)
+plotMA(deseq2Results)          
+
+# Load libraries
+# install.packages(c("ggplot2", "scales", "viridis"))
+library(ggplot2)
+library(scales) # needed for oob parameter
+library(viridis)
+
+# Coerce to a data frame
+deseq2ResDF <- as.data.frame(deseq2Results)
+
+# Examine this data frame
+head(deseq2ResDF)
+
+# Set a boolean column for significance
+deseq2ResDF$significant <- ifelse(deseq2ResDF$padj < .1, "Significant", NA)
+
+# Plot the results similar to DEseq2
+ggplot(deseq2ResDF, aes(baseMean, log2FoldChange, colour=significant)) + geom_point(size=1) + scale_y_continuous(limits=c(-3, 3), oob=squish) + scale_x_log10() + geom_hline(yintercept = 0, colour="tomato1", size=2) + labs(x="mean of normalized counts", y="log fold change") + scale_colour_manual(name="q-value", values=("Significant"="red"), na.value="grey50") + theme_bw()
+
+# Let's add some more detail
+ggplot(deseq2ResDF, aes(baseMean, log2FoldChange, colour=padj)) + geom_point(size=1) + scale_y_continuous(limits=c(-3, 3), oob=squish) + scale_x_log10() + geom_hline(yintercept = 0, colour="darkorchid4", size=1, linetype="longdash") + labs(x="mean of normalized counts", y="log fold change") + scale_colour_viridis(direction=-1, trans='sqrt') + theme_bw() + geom_density_2d(colour="black", size=2)
+
+# Transform count data using the variance stablilizing transform
+deseq2VST <- vst(deseq2Data)
+
+# Convert the DESeq transformed object to a data frame
+deseq2VST <- assay(deseq2VST)
+deseq2VST <- as.data.frame(deseq2VST)
+deseq2VST$Gene <- rownames(deseq2VST)
+head(deseq2VST)
+
+# Keep only the significantly differentiated genes where the fold-change was at least 3
+sigGenes <- rownames(deseq2ResDF[deseq2ResDF$padj <= .05 & abs(deseq2ResDF$log2FoldChange) > 3,])
+deseq2VST <- deseq2VST[deseq2VST$Gene %in% sigGenes,]
+
+# Convert the VST counts to long format for ggplot2
+library(reshape2)
+
+# First compare wide vs long version
+deseq2VST_wide <- deseq2VST
+deseq2VST_long <- melt(deseq2VST, id.vars=c("Gene"))
+
+head(deseq2VST_wide)
+head(deseq2VST_long)
+
+# Now overwrite our original data frame with the long format
+deseq2VST <- melt(deseq2VST, id.vars=c("Gene"))
+
+# Make a heatmap
+heatmap <- ggplot(deseq2VST, aes(x=variable, y=Gene, fill=value)) + geom_raster() + scale_fill_viridis(trans="sqrt") + theme(axis.text.x=element_text(angle=65, hjust=1), axis.text.y=element_blank(), axis.ticks.y=element_blank())
+heatmap
+
+
+# Convert the significant genes back to a matrix for clustering
+deseq2VSTMatrix <- dcast(deseq2VST, Gene ~ variable)
+rownames(deseq2VSTMatrix) <- deseq2VSTMatrix$Gene
+deseq2VSTMatrix$Gene <- NULL
+
+# Compute a distance calculation on both dimensions of the matrix
+distanceGene <- dist(deseq2VSTMatrix)
+distanceSample <- dist(t(deseq2VSTMatrix))
+
+# Cluster based on the distance calculations
+clusterGene <- hclust(distanceGene, method="average")
+clusterSample <- hclust(distanceSample, method="average")
+
+# Construct a dendogram for samples
+install.packages("ggdendro")
+library(ggdendro)
+sampleModel <- as.dendrogram(clusterSample)
+sampleDendrogramData <- segment(dendro_data(sampleModel, type = "rectangle"))
+sampleDendrogram <- ggplot(sampleDendrogramData) + geom_segment(aes(x = x, y = y, xend = xend, yend = yend)) + theme_dendro()
+
+# Re-factor samples for ggplot2
+deseq2VST$variable <- factor(deseq2VST$variable, levels=clusterSample$labels[clusterSample$order])
+
+# Construct the heatmap. note that at this point we have only clustered the samples NOT the genes
+heatmap <- ggplot(deseq2VST, aes(x=variable, y=Gene, fill=value)) + geom_raster() + scale_fill_viridis(trans="sqrt") + theme(axis.text.x=element_text(angle=65, hjust=1), axis.text.y=element_blank(), axis.ticks.y=element_blank())
+heatmap
+
+# Combine the dendrogram and the heatmap
+install.packages("gridExtra")
+library(gridExtra)
+grid.arrange(sampleDendrogram, heatmap, ncol=1, heights=c(1,5))
+
+
+# Load in libraries necessary for modifying plots
+#install.packages("gtable")
+library(gtable)
+library(grid)
+
+# Modify the ggplot objects
+sampleDendrogram_1 <- sampleDendrogram + scale_x_continuous(expand=c(.0085, .0085)) + scale_y_continuous(expand=c(0, 0))
+heatmap_1 <- heatmap + scale_x_discrete(expand=c(0, 0)) + scale_y_discrete(expand=c(0, 0))
+
+# Convert both grid based objects to grobs
+sampleDendrogramGrob <- ggplotGrob(sampleDendrogram_1)
+heatmapGrob <- ggplotGrob(heatmap_1)
+
+# Check the widths of each grob
+sampleDendrogramGrob$widths
+heatmapGrob$widths
+
+# Add in the missing columns
+sampleDendrogramGrob <- gtable_add_cols(sampleDendrogramGrob, heatmapGrob$widths[7], 6)
+sampleDendrogramGrob <- gtable_add_cols(sampleDendrogramGrob, heatmapGrob$widths[8], 7)
+
+# Make sure every width between the two grobs is the same
+maxWidth <- unit.pmax(sampleDendrogramGrob$widths, heatmapGrob$widths)
+sampleDendrogramGrob$widths <- as.list(maxWidth)
+heatmapGrob$widths <- as.list(maxWidth)
+
+# Arrange the grobs into a plot
+finalGrob <- arrangeGrob(sampleDendrogramGrob, heatmapGrob, ncol=1, heights=c(2,5))
+
+# Draw the plot
+grid.draw(finalGrob)
